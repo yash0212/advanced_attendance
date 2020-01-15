660c8210b7df757276817d62a6363c6625f18d65
Yash Lotan <yashlotan7@gmail.com>
2020-01-15 11:41:06 +0530

    App icon and persistant strage added

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-import {store} from './src/redux/store';
+import {store, persistor} from './src/redux/store';
 import {Provider as StoreProvider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
 import React from 'react';
+import {PersistGate} from 'redux-persist/integration/react';
+
 const RootStack = createSwitchNavigator(
   {
     Login: LoginScreen,
@@ -23,9 +25,11 @@ export default class App extends React.Component {
   render() {
     return (
       <StoreProvider store={store}>
-        <PaperProvider>
-          <Navigation />
-        </PaperProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <PaperProvider>
+            <Navigation />
+          </PaperProvider>
+        </PersistGate>
       </StoreProvider>
     );
   }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import reducer from './reducer';
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
